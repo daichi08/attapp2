be5e4b3346f155f6a68b86fc22031ba7ce32b458
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2021-12-16 09:07:49 +0900

    delete sigin&up func in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,4 @@
-import { signUp, signIn } from '../lib/auth';
-
 const Home = () : JSX.Element => {
-  // const createAccount = signUp("hoge@hoge.hogea", "hogehogea");
-  // const logIn = signIn("hoge@hoge.hogea", "hogehogea");
-
   return (
     <div>
       Hello, World!
